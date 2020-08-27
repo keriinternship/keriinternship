7e9ca5fa1cb8450253767019a67219612892c96a
keriinternship <internship@keri.vn>
2020-08-27 10:26:40 +0700

    Update Ql_planes
    Hao update folder QL_planes

---
+++ QL_Planes/api_customer.php
@@ -0,0 +1,23 @@
+<?php
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PASSWORD', '');
+DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_NAME', 'qlplanes');
+$mysqli = @mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) OR die('Could not connect to MySQL');
+@mysql_select_db(DB_NAME) OR die('Could not select the database');
+mysql_query("SET NAMES 'utf8'");
+ 
+$query = "SELECT * FROM `customer`";
+$resouter = mysql_query($query);
+ 
+$temparray = array();
+$total_records = mysql_num_rows($resouter);
+ 
+if ($total_records >= 1) {
+    while ($row = mysql_fetch_assoc($resouter)) {
+        $temparray[] = $row;
+    } 
+}   
+echo json_encode($temparray);
+ 
+?>
